44715a76bfc8f3611c2d37d1ab57a1aabe455f17
Priyanshu <priyanshu9836@gmail.com>
2023-04-28 16:11:06 +0530

    added crud operation

--- index.js
+++ index.js
@@ -3,65 +3,101 @@ const users= require("./MOCK_DATA.json")
 const mongoose = require ("mongoose")
 const app = express()
 const port = 3000
+const fs = require('fs');
 
 
+//mogodb connecting
+mongoose.connect("mongodb://127.0.0.1:27017/usersDatabase")
+.then(()=>{console.log("db is connected")})
+.catch((e)=>console.log("error",e))
 
-// mongoose.connect("mongodb://127.0.0.1:27017/usersDb")
-// .then(()=>{console.log("db is connected")})
-// .catch((e)=>console.log("error",e))
-// const userSchema=new mongoose.Schema({
-//   firstName:{
-//     type:String,
-//     required:true,
-//   },
-//   lastName:{
-//     type:String,
-   
-//   },
-//   email:{
-//     type:String,
-//     required:true,
-//     unique:true,
-//   },
-//   jobtitle:{
-//     type:String,
-//   },
-//   gender:{
-//     type:String,
-//     required:true,
-//   },
-// })
+const userSchema=new mongoose.Schema({
+  firstName:{
+    type:String,
+    required:true,
+  },
+  lastName:{
+    type:String,
+  },
+  email:{
+    type:String,
+    required:true,
+    unique:true,
+  },
+  jobtitle:{
+    type:String,
+  },
+  gender:{
+    type:String,
+    required:true,
+  },
+},{timestamps:true})
 
-// const User=mongoose.model("user",userSchema)
+const User=mongoose.model("user",userSchema)
+
+app.use(express.urlencoded({extended:false}))
 
 
-app.get('/api/users', (req, res) => {
-  console.log(req.query)
-  return res.send(users)
+app.get('/api/users', async(req, res) => {
+  const allDbUsers=await User.find({})
+  //console.log(req.query)
+  return res.send(allDbUsers)
 })
 
 
 
 app
 .route("/api/users/:id")
-.get( (req,res) =>{
-  const id =Number(req.params.id);
-  // console.log(id);
-  const user=users.find((user)=>user.id===id);
+
+.get(async (req,res) =>{
+ const user=await User.findById(req.params.id);
   return res.send(user);}
 )
-.post((req, res) => {
-  console.log(req.query)
-  return res.send(users)
+
+.patch(async(req, res) => {
+  const data=req.body
+  console.log(data)
+  return res.send("Updated")
 })
-.patch((req, res) => {
-  console.log(req.query)
-  return res.send(users)
+
+.delete(async(req, res) => {
+  await User.findByIdAndDelete(req.params.id);
+  return res.json("Deleted")
 })
-.delete((req, res) => {
-  console.log(req.query)
-  return res.send(users)
+
+
+
+app
+.route("/api/users/")
+
+// .post((req, res) => {
+//   const body=req.body;
+//   console.log(body,users.length);
+//   const id=users.lenght+1;
+//   users.push({id:users.length +1,...body});
+//   fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
+//   return res.json({id:users.length});
+// })
+// })
+
+.post(async(req, res) => {
+  const body=req.body;
+ if(!body.first_name || !body.last_name|| !body.email|| !body.gender|| !body.job_title){
+  return res.send("all data must be filled");
+ }
+ const result=await User.create(
+  { firstName: body.first_name,
+    lastName: body.last_name,
+    email:  body.email,
+    gender: body.gender,
+    jobtitle:  body.job_title
+       }
+ );
+ console.log("result",result);
+ return res.status(201).json({msg:"success"})
 })
+
+
 
 
 app.listen(port, () => {
